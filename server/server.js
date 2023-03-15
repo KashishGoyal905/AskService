0b9609e431ec897c449daf891617736c76ca2e4e
kunal7557 <kunal3558.be20@chitkara.edu.in>
2023-03-15 11:13:05 +0530

    server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+const port  = 8000;
+
+app.get('/',(req,res)=>{
+    res.send('Hello m here')
+})
+
+
+app.listen(port,(req,res)=>{
+    console.log(`server is running on ${port}`);
+})
