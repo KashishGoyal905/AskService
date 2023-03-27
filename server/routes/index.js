0febe8834e22a64980b069abdb90dbd3a6ea60e3
kunal7557 <kunal3558.be20@chitkara.edu.in>
2023-03-27 10:14:01 +0530

    front end back end connection

---
+++ server/routes/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const router = express.Router();
+const User = require("../models/client/clientschema");
+
+//routes
+
+router.get("/home", (req, res, next) => {
+  res.send("welcome");
+});
+
+router.post("/login", (req, res) => {
+  res.send("welcome login");
+});
+router.post("/register", (req, res) => {
+  console.log("register");
+  const user = new User({
+    userName:req.body.userName,
+    email:req.body.email,
+    password:req.body.password,
+  });
+
+  res.send(user);
+
+  // user.save((err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     res.send({ message: "Successfully Registered" });
+  //   }
+  // });
+});
+
+module.exports = router;
